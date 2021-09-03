a2ffa437b41569668f264a0f223e1438a71d9a41
talktoaht <90051622+talktoaht@users.noreply.github.com>
2021-09-04 00:26:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ const CONFIG = {
     btnIntro: 'Reply 2021',
     title: 'Hỏi nhỏ nè: Mia có quý tui hông? 😙',
     desc: 'From FTU to ULIS ',
-    btnYes: 'Dạ có nhưng tui ngại nói lắm',
-    btnNo: 'Không, đồ đáng ghéc',
+    btnYes: 'Dạ có, nhưng tui ngại nói lắm',
+    btnNo: 'Không, đồ đáng ghéc, quen biết gì nhau',
     question: 'Trên thế giới hơn 7 tỉ người sao tui va phải Mia theo một cách thật... kì lạ?',
     btnReply: 'Lời nhắn này đã đến nơi cần đến :>',
     reply: 'Bởi vì Mia xứng đáng với điều tốt đẹp nhất, đó là tui :)))))',
     mess: 'Phấn đấu, phấn đấu x 100, be young and rich :v',
-    messDesc: 'Hẹn Mia bao giờ hết dãn cách...',
-    btnAccept: 'Đi',
+    messDesc: 'Hẹn Mia bao giờ hết dãn cách tui đưa Mia đi chơi ha...',
+    btnAccept: 'Bao giờ Mia sẵn sàng thì lên tiếng nha',
     messLink: 'https://www.facebook.com/hoang.esther.58' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
